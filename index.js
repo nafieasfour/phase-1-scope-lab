216aedc943acf714875eedeca742b54a431a2334
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-13 12:28:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+   bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Alice";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Bob"; 
+}
